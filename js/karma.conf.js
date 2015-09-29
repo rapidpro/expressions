1f3223209d9c1739a99094e6701eb06815657b3b
Rowan Seymour <rowanseymour@gmail.com>
2015-09-29 16:49:15 +0200

    Fix for .travis.yml

--- js/karma.conf.js
+++ js/karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-        'dist/excellent.js',
+        'excellent.js',
         'tests.js'
     ],
 
