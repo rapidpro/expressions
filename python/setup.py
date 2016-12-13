38149173025a1fa708c083a27ec3e2fee2efceb1
Norbert Kwizera <norkans7@gmail.com>
2016-12-13 22:05:40 +0200

    adjust to parse correctly the conditional packages required and increment python version

--- python/setup.py
+++ python/setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup, find_packages
+from collections import defaultdict
+
+
+extra_packages = defaultdict(list)
 
 
 def _is_requirement(line):
@@ -7,21 +11,28 @@ def _is_requirement(line):
     return line and not (line.startswith("-r") or line.startswith("#"))
 
 
-def _read_requirements(filename):
+def _read_requirements(filename, extra_packages):
     """Returns a list of package requirements read from the file."""
     requirements_file = open(filename).read()
-    return [line.strip() for line in requirements_file.splitlines()
-            if _is_requirement(line)]
 
+    hard_requirements = []
+    for line in requirements_file.splitlines():
+        if _is_requirement(line):
+            if line.find(';') > -1:
+                dep, condition = tuple(line.split(';'))
+                extra_packages[condition.strip()].append(dep.strip())
+            else:
+                hard_requirements.append(line.strip())
+    return hard_requirements, extra_packages
 
-required_packages = _read_requirements("requirements/base.txt")
-test_packages = _read_requirements("requirements/tests.txt")
+required_packages, extra_packages = _read_requirements("requirements/base.txt", extra_packages)
+test_packages, extra_packages = _read_requirements("requirements/tests.txt", extra_packages)
 
 setup(
     name='rapidpro-expressions',
-    version='1.1.8',
+    version='1.1.9',
     description='Python implementation of the RapidPro expression and templating system',
-    url='https://github.com/rapidpro/flows',
+    url='https://github.com/rapidpro/expressions',
 
     author='Nyaruka',
     author_email='code@nyaruka.com',
@@ -40,7 +51,7 @@ setup(
     packages=find_packages(),
     package_data={'temba_expressions': ['month.aliases']},
     install_requires=required_packages,
-
+    extra_packages=extra_packages,
     test_suite='nose.collector',
     tests_require=required_packages + test_packages,
 )
