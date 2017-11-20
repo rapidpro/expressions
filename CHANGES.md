eb6851c435e0a9db4aa2e4f88b6a0844efaa3aba
Rowan Seymour <rowanseymour@gmail.com>
2017-11-20 16:31:13 -0500

    Prep for 1.4.1 release (Python)

---
+++ CHANGES.md
@@ -0,0 +1,3 @@
+v1.4.1 (2017-11-20)
+==================
+ * Fix parsing of times with AM/PM markers (https://github.com/rapidpro/expressions/pull/24)
--- python/setup.py
+++ python/setup.py
@@ -30,7 +30,7 @@ test_packages, extra_packages = _read_requirements("requirements/tests.txt", ext
 
 setup(
     name='rapidpro-expressions',
-    version='1.4',
+    version='1.4.1',
     description='Python implementation of the RapidPro expression and templating system',
     url='https://github.com/rapidpro/expressions',
 
